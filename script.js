7f052e35aacd2ccfd110c54f04edd02b74ef5e80
Vishal17-coder <vishalwale17@gmail.com>
2025-04-20 17:55:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Student at Vishwakarma Institute of Technology Pune.", "Frontend Developer.", "eSports Volunteer At Saarthi Club.", "Tech Enthusiast."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true
+});
